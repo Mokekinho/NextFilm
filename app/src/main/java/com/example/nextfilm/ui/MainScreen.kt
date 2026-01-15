e6dfa5d42bbbbae6b69dc23ca25932421e74d6fa
Moises Protazio <moisesprotazio@gmail.com>
2026-01-14 21:45:57 -0300

    feat: add a topBar on the MainScreen Scaffold

--- app/src/main/java/com/example/nextfilm/ui/MainScreen.kt
+++ app/src/main/java/com/example/nextfilm/ui/MainScreen.kt
@@ -1,12 +1,25 @@
 package com.example.nextfilm.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.nextfilm.navigation.AppNavigation
 
@@ -21,6 +34,64 @@ fun MainScreen(
             modifier = modifier
                 .fillMaxSize()
             ,
+            topBar = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                listOf(
+                                    MaterialTheme.colorScheme.background,
+                                    Color.Transparent
+                                )
+                            )
+                        )
+                ){
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .windowInsetsPadding(WindowInsets.statusBars) //considera o padding da camera
+
+                        ,
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        Text(
+                            text = "Home",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier
+                                .clickable(
+                                    onClick = {
+
+                                    }
+                                )
+                        )
+                        Text(
+                            text = "Series",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier
+                                .clickable(
+                                    onClick = {
+
+                                    }
+                                )
+                        )
+                        Text(
+                            text = "Movies",
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier
+                                .clickable(
+                                    onClick = {
+
+                                    }
+                                )
+                        )
+
+
+                    }
+                }
+
+            }
 
             ) {
             it // vou escrever isso aqui pro nao ter que usar o padding
